d5a8e2b3377c82df5c76c6ade67933881b818eeb
jailsonsf <jailson.s.f@outlook.com>
2022-09-29 14:40:43 -0300

    Add foreign_key user_id and create Procfile to Heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_29_130714) do
+ActiveRecord::Schema.define(version: 2022_09_29_172119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_09_29_130714) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "users"
 end
